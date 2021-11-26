722627521d9e1a882c0a93242c6a6d2ae63a6d64
Corey Fisher <coreyfisher2021@gmail.com>
2021-11-26 13:22:25 -0600

    commit tree 473664a9872ad2f96dafab77605c2bb7196c0499

--- src/index.js
+++ src/index.js
@@ -12,4 +12,82 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  toys();
+  appendNewToy();
 });
+
+function renderToys(toy) {
+  let card = document.createElement("div")
+  let collection = document.getElementById("toy-collection");
+  let picture = document.createElement("img");
+  let likes = document.createElement("p");
+  let liker = document.createElement("button");
+  liker.className = "like-btn";
+  liker.id = toy.id;
+  liker.textContent = "Like"
+  likes.textContent = `${toy.likes} Likes`;
+  likes.className = "like"
+  picture.src = toy.image;
+  picture.className = "toy-avatar";
+  card.className = "card";
+  card.textContent = toy.name;
+  collection.appendChild(card);
+  card.appendChild(picture);
+  card.appendChild(likes);
+  card.appendChild(liker);
+  liker.addEventListener("click", () => {
+    fetch(url = `http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers:
+      {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        "likes": `${toy.likes++}`
+      })
+    })
+    likes.textContent = `${toy.likes} Likes`
+  })
+
+}
+const toyName =   `${document.getElementsByClassName("input-text")[0].value}`
+const toyImage = `${document.getElementsByClassName("input-text")[1].value}`
+
+function newToy() {
+  fetch(url = `http://localhost:3000/toys`, {
+      method: "POST",
+      headers:
+      {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "name": `${document.getElementsByClassName("input-text")[0].value}`,
+        "image": `${document.getElementsByClassName("input-text")[1].value}`,
+        "likes": "0",
+      })
+    })
+
+}
+
+function toys() {
+  fetch("http://localhost:3000/toys")
+    .then(res => res.json())
+    .then(toy => toy.forEach(toy => renderToys(toy)));
+}
+
+
+function removeToy() {
+  fetch(url = "http://localhost:3000/toys/", {
+    method: "DELETE",
+  })
+}
+
+
+function appendNewToy() {
+  const submit = document.querySelector("input.submit")
+  submit.addEventListener("click", function(e) {
+    e.preventDefault()
+    newToy()
+  })
+}
